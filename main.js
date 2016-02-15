19e34f75789c95b6b2148fa580eac28c2793b0cf
Nanditha Sreenivasan <nanditha.svn@gmail.com>
2016-02-15 17:50:12 -0500

    Use express to fix heroku port error.

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var Twit = require('twit');
 var Promise = require('bluebird');
+var Express = require('express');
+
+var app     = Express();
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 var T = new Twit({
     consumer_key:         process.env.CONSUMER_KEY,
